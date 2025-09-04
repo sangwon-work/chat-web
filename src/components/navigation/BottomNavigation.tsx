f3ad396153520afe11ca33110afedf3519f35e9f
choisangwon <rngus3698@naver.com>
2025-09-04 10:45:43 +0900

    네비게이션 ui 수정

--- src/components/navigation/BottomNavigation.tsx
+++ src/components/navigation/BottomNavigation.tsx
@@ -28,7 +28,7 @@ export default function BottomNavigation({ navHeight = 64 }: Props) {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed bottom-0 h-15 left-0 right-0 bg-white border-t shadow-sm flex justify-around py-2 z-50">
+    <nav className="fixed bottom-0 h-15 left-0 right-0 bg-gray-100 shadow-sm flex justify-around py-2">
       {navItems.map((item) => {
         const isActive = pathname.startsWith(item.href);
 
@@ -37,7 +37,7 @@ export default function BottomNavigation({ navHeight = 64 }: Props) {
             key={item.href}
             href={item.href}
             className={`flex flex-col items-center gap-1 text-sm ${
-              isActive ? 'text-blue-500' : 'text-gray-500'
+              isActive ? 'text-black' : 'text-gray-500'
             }`}
           >
             {item.icon}
